fba063b2200754a4722e2e9e4dbd8993b9eaebe1
Rudimar Ronsoni <rudimar@outlook.com>
2026-01-28 20:15:54 +0100

    feat: enhance StaticProxySource to support proxy configuration with username and password, and improve logging for ignored entries

--- src/Core/Ghost/Core/ProxyOptions.cs
+++ src/Core/Ghost/Core/ProxyOptions.cs
@@ -12,6 +12,9 @@ public class ProxyOptions
 public class StaticProxyConfig
 {
     public bool Enabled { get; set; }
+    public int? Port { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
     public List<string> Items { get; set; } = new();
 }
 
