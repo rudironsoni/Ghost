aa0e6d637d03f4d068a021802528341f2920c6c5
Rudimar Ronsoni <rudimar@outlook.com>
2026-01-29 11:23:35 +0100

    Configure Indeed and Glassdoor HttpClients to use RotatingWebProxy via ConfigurePrimaryHttpMessageHandler

--- src/Platforms/Ghost.Platform.Glassdoor/GlassdoorExtension.cs
+++ src/Platforms/Ghost.Platform.Glassdoor/GlassdoorExtension.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ghost.Hosting;
+using Ghost.Abstractions;
+using Ghost.Http;
+using System.Net.Http;
 
 namespace Ghost.Platform.Glassdoor;
 
@@ -11,10 +14,20 @@ public sealed class GlassdoorExtension : IExtension
     public IReadOnlyList<Type> ProvidedServices => new[] { typeof(Ghost.Contracts.Jobs.IJobClient) };
     public IReadOnlyList<Type> RequiredServices => Array.Empty<Type>();
 
-    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
-    {
-        services.Configure<GlassdoorOptions>(configuration.GetSection("Ghost:Extensions:Glassdoor"));
-        services.AddHttpClient<Internal.GlassdoorApiClient>();
-        services.AddScoped<Ghost.Contracts.Jobs.IJobClient, GlassdoorJobClient>();
-    }
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<GlassdoorOptions>(configuration.GetSection("Ghost:Extensions:Glassdoor"));
+            services.AddHttpClient<Internal.GlassdoorApiClient>()
+                .ConfigurePrimaryHttpMessageHandler(sp =>
+                {
+                    var provider = sp.GetRequiredService<IProxyProvider>();
+                    return new HttpClientHandler
+                    {
+                        Proxy = new RotatingWebProxy(provider),
+                        UseProxy = true
+                    };
+                });
+
+            services.AddScoped<Ghost.Contracts.Jobs.IJobClient, GlassdoorJobClient>();
+        }
 }
--- src/Platforms/Ghost.Platform.Indeed/IndeedExtension.cs
+++ src/Platforms/Ghost.Platform.Indeed/IndeedExtension.cs
@@ -3,25 +3,38 @@ using Ghost.Models;
 using Microsoft.Extensions.Configuration;
 using Ghost.Platform.Indeed.Internal;
 using Ghost.Contracts.Jobs;
+using Ghost.Abstractions;
+using Ghost.Http;
+using System.Net.Http;
 
 namespace Ghost.Platform.Indeed;
 
 public static class IndeedExtension
 {
-    public static IServiceCollection AddIndeed(this IServiceCollection services, IConfiguration config)
-    {
-        services.Configure<IndeedOptions>(config.GetSection("Indeed"));
-        var opts = config.GetSection("Indeed").Get<IndeedOptions>() ?? new IndeedOptions();
+        public static IServiceCollection AddIndeed(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<IndeedOptions>(config.GetSection("Indeed"));
+            var opts = config.GetSection("Indeed").Get<IndeedOptions>() ?? new IndeedOptions();
 
-        if (!opts.Enabled) return services;
+            if (!opts.Enabled) return services;
 
-        // register IndeedOptions for ApiClient constructor
-        services.AddSingleton(opts);
+            // register IndeedOptions for ApiClient constructor
+            services.AddSingleton(opts);
 
-        services.AddHttpClient<IndeedApiClient>(client => { client.Timeout = System.TimeSpan.FromSeconds(30); });
-        services.AddSingleton<IndeedJobClient>();
-        services.AddSingleton<IJobClient>(sp => sp.GetRequiredService<IndeedJobClient>());
+            services.AddHttpClient<IndeedApiClient>(client => { client.Timeout = System.TimeSpan.FromSeconds(30); })
+                .ConfigurePrimaryHttpMessageHandler(sp =>
+                {
+                    var provider = sp.GetRequiredService<IProxyProvider>();
+                    return new HttpClientHandler
+                    {
+                        Proxy = new RotatingWebProxy(provider),
+                        UseProxy = true
+                    };
+                });
 
-        return services;
-    }
+            services.AddSingleton<IndeedJobClient>();
+            services.AddSingleton<IJobClient>(sp => sp.GetRequiredService<IndeedJobClient>());
+
+            return services;
+        }
 }
