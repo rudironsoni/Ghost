39a27f0f9bc9df06a2d472e5943c1c08efe8bf33
Rudimar Ronsoni <rudimar@outlook.com>
2026-01-29 11:47:45 +0100

    fix: Specify full namespace for IJobClient and IJobScraper registrations in AddGhostAggregator

--- src/Core/Ghost/Extensions/ServiceCollectionExtensions.cs
+++ src/Core/Ghost/Extensions/ServiceCollectionExtensions.cs
@@ -10,8 +10,9 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddGhostAggregator(this IServiceCollection services)
     {
         services.AddScoped<AggregatedJobClient>();
-        services.AddScoped<IJobClient>(sp => sp.GetRequiredService<AggregatedJobClient>());
-        services.AddScoped<IJobScraper>(sp => sp.GetRequiredService<AggregatedJobClient>());
+        services.AddScoped<Ghost.Contracts.Jobs.IJobClient>(sp => sp.GetRequiredService<AggregatedJobClient>());
+        // AggregatedJobClient implements IJobClient; register as IJobScraper as well by wrapping
+        services.AddScoped<Ghost.Abstractions.IJobScraper>(sp => sp.GetRequiredService<AggregatedJobClient>() as Ghost.Abstractions.IJobScraper ?? throw new InvalidOperationException("AggregatedJobClient is not an IJobScraper"));
         return services;
     }
 }
